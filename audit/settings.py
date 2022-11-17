f1f3284152fa6ff9353b0b63d0bc948765d54c53
kevin kibebe <kibebekevin@gmail.com>
2022-11-18 00:51:25 +0300

    updated

--- audit/settings.py
+++ audit/settings.py
@@ -80,33 +80,46 @@ WSGI_APPLICATION = 'audit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#    'default': 
+#             {
+
+#                 'ENGINE': 'mysql.connector.django',
+#                 'NAME':'myauditer',
+#                 'USER':'root',
+#                 'PASSWORD':'',
+#                 'HOST':'localhost',
+#                 'PORT':3306,
+#                 'OPTIONS':
+#                 {
+#                     'autocommit':True,
+#                 },
+#             }
+# }
 
 DATABASES = {
-   'default': 
+    'default': 
             {
 
-                'ENGINE': 'mysql.connector.django',
-                'NAME':'myauditer',
-                'USER':'root',
-                'PASSWORD':'',
-                'HOST':'localhost',
-                'PORT':3306,
-                'OPTIONS':
-                {
-                    'autocommit':True,
-                },
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME':env('DATABASE_NAME'),
+                'USER':env('DATABASE_USER'),
+                'PASSWORD':env('DATABASE_PASSWORD'),
+                'HOST':env('DATABASE_HOST'),
+                'PORT':env('DATABASE_PORT'),
             }
 }
 
-# import dj_database_url
-# db_from_env=dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env=dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
