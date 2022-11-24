aafdccad5f3ed9586ade5ee3638bdc07aab8dade
kevin kibebe <kibebekevin@gmail.com>
2022-11-24 09:51:11 +0300

    updated

--- audit/settings.py
+++ audit/settings.py
@@ -15,10 +15,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG:
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['*']
 else:
     ALLOWED_HOSTS = ['localhost','127.0.0.1','192.168.43.106','myauditer.herokuapp.com','www.myauditer.herokuapp.com']
 
